bdda514d1d581abbf376f7d52cebc0993281891e
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-12-26 10:28:59 +0300

    Initialized hw3

---
+++ PCPartsSystem/src/PartsStore.java
@@ -0,0 +1,24 @@
+public class PartsStore {
+  public PartsStore(){
+
+  }
+  public void FindPartsWithBrand(String type, String brand){
+
+  }
+
+  public void TotalPrice(String type, String brand, String model){
+
+  }
+
+  public void UpdateStock(){
+
+  }
+
+  public void FindCheapestMemory(int capacity){
+
+  }
+
+  public void FindFastestCPU(){
+
+  }
+}
